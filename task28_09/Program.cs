a7444648ddebde39ab2670ea9ba7c8236858fb5a
JalilovYusifJY2004 <yusifcelilli2004@gmail.com>
2023-09-28 22:50:43 +0400

    add

---
+++ task28_09/Program.cs
@@ -0,0 +1,39 @@
+﻿//Task 1
+
+//int[] ages = {1,5,7,8 };
+//int sum = 0;
+//for (int i = 0; i < ages.Length; i++)
+//{
+//    sum += ages[i];
+//}
+//Console.WriteLine(sum);
+
+
+//Task 2
+
+//string[] words = { "yusif", "adil", "xeyal", "eli" };
+//foreach (string word in words)
+//    for (int i = 0; i < words.Length; i++)
+//    {
+//        if (words.Length > 4)
+//        {
+//            Console.WriteLine(words[i]);
+//        }
+//    }
+
+//Task 3
+
+int[] num = new int[] { 1, 9, 6, 7, 5, 9 };
+for (int i = 0; i < num.Length-1; i--) 
+{
+    Console.WriteLine(num[i]);
+}
+
+
+
+
+
+
+
+
+
